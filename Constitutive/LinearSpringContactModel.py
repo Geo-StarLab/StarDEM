2058b509c4f1c55c0787323f2172cc8f035bb133
Chengshun Shang <chengshun.shang1996@gmail.com>
2022-04-02 13:43:15 +0200

    update readme.md

---
+++ Constitutive/LinearSpringContactModel.py
@@ -0,0 +1,9 @@
+# ---------------Name: PyDEM -------------------
+# ----------Author: Chengshun Shang-------------
+# ---------------Date: 04-01-2022---------------
+# -------------License : MIT license------------
+
+class LinearSpringContactModel():
+
+    def __init__(self) -> None:
+        pass
